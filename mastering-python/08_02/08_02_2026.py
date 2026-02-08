051d57a5f33d9ab6be043f9ce0d310e72d67dc50
rajbeer <rajbeer.singh>
2026-02-08 10:02:14 +0530

    updated

---
+++ mastering-python/08_02/08_02_2026.py
@@ -0,0 +1,10 @@
+print("Enter Name: ", end = " ")
+name = input()
+print("Enter Email: , ", end = " ")
+email = input()
+print("Enter your mobile numer: ", end = " ")
+number = input()
+print(name, email, number )
+
+
+# uv run python 0
